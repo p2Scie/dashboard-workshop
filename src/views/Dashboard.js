d8bab24ae9c69f232f61b7ce878f2c964f84991e
Stevens <sdemervil@gmail.com>
2021-12-16 17:26:31 +0100

    base

---
+++ src/views/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {BarChart} from "../components/BarChart";
+
+export const Dashboard = () => {
+    return  (
+        <div>
+            <h1>Dashboard Empreinte Carbone </h1>
+            <BarChart />
+        </div>
+    )
+}
