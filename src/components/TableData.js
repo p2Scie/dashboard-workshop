55bda58c7ff0808b487e6188a7a97af558e97daf
Abdelilah LEZAR <abdelilahlezar@gmail.com>
2021-12-16 17:47:41 +0100

    Commit 1

---
+++ src/components/TableData.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TableData = () => {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
+export default TableData;
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BarChart} from "../components/BarChart";
+import TableData from "../components/TableData";
 
 export const Dashboard = () => {
     return  (
         <div>
             <h1>Dashboard Empreinte Carbone </h1>
             <BarChart />
+            <TableData />
         </div>
     )
 }
